ebc19775e4f29c8a1b743d5b60a09c169c887221
Miquel <mpec001@gmail.com>
2025-10-08 10:26:49 +0200

    Afegit paquet i classe Modificacio1

---
+++ EDProva1/src/modificacio1/Modificacio1.java
@@ -0,0 +1,10 @@
+package modificacio1;
+
+public class Modificacio1 {
+
+	public static void main(String[] args) {
+		System.out.println("Nova classe dins el package de les modificacion");
+
+	}
+
+}
