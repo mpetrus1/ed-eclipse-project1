5b7d4e837e29bf43c92e77a7d8e81fd33fa3b16c
Miquel <mpec001@gmail.com>
2025-10-08 10:05:11 +0200

    Tercera linia

--- EDProva1/src/Principal.java
+++ EDProva1/src/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mon java-git-eclipse");
 		System.out.println("Primer projecte amb git eclipse");
+		System.out.println("Modificacions sobre el mateix fitxer");
 
 	}
 
