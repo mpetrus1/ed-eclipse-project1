549d344864e35b540d957cd1f3fca27bf2cc67df
Miquel <mpec001@gmail.com>
2025-10-08 10:25:17 +0200

    Afegida 4a linia a la classe Principal

--- EDProva1/src/Principal.java
+++ EDProva1/src/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 		System.out.println("Hola mon java-git-eclipse");
 		System.out.println("Primer projecte amb git eclipse");
 		System.out.println("Modificacions sobre el mateix fitxer");
+		System.out.println("Afegim la 4a linia desde la nova branca");
 
 	}
 
